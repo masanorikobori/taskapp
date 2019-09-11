7ad1e61c66af9c87c01f59a31fa38cda127cfdbf
kobori_kawaguchi <kobori@allphoto.sakura.ne.jp>
2019-09-11 17:57:37 +0900

    9/11 17:57

---
+++ taskapp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 小堀まさのり on 2019/09/11.
+//  Copyright © 2019 Masanori Kobori. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {  //プライマリーキーとはデータベースでそれぞれのデータを一意に識別するためのID
+        return "id"
+    }
+}
+
