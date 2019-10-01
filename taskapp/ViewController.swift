d76e2b92152442fb1c3c5a2196875f3949da4266
kobori_kawaguchi <kobori@allphoto.sakura.ne.jp>
2019-10-02 08:30:53 +0900

    10/2 8:30

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -16,6 +16,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var tableView: UITableView!  //テーブルビューをアウトレットした
     
+    // ▼▼▼ここから検索窓のコード▼▼▼
+    @IBOutlet weak var testSearchBar: UISearchBar!  //検索窓をアウトレットした
+    //↓データ
+    let dataList = ["月刊コロコロコミック（小学館）",
+                    "コロコロイチバン！（小学館）",
+                    "最強ジャンプ（集英社）",
+                    "Vジャンプ（集英社）",
+                    "週刊少年サンデー（小学館）",
+                    "週刊少年マガジン（講談社）",
+                    "週刊少年ジャンプ（集英社）",
+                    "週刊少年チャンピオン（秋田書店）",
+                    "月刊少年マガジン（講談社）",
+                    "月刊少年チャンピオン（秋田書店）",
+                    "月刊少年ガンガン（スクウェア）",
+                    "月刊少年エース（KADOKAWA）",
+                    "月刊少年シリウス（講談社）",
+                    "週刊ヤングジャンプ（集英社）",
+                    "ビッグコミックスピリッツ（小学館）",
+                    "週刊ヤングマガジン（講談社）"]
+    
+    //データを返すメソッド
+    private func tableView(tableView:UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
+        
+        //セルを取得する。
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for:indexPath as IndexPath) as UITableViewCell
+        
+        cell.textLabel?.text = dataList[indexPath.row]
+        return cell
+    }
+    
+    //データの個数を返すメソッド
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return dataList.count
+    }
+    // ▲▲▲検索窓ここまで▲▲▲
+    
+    
+
+    
+    
     //　Realm(レルム)のインスタンス？？
     lazy var realm:Realm = {
         return try! Realm()
